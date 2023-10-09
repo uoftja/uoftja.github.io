3e4b46606fff128caf5bab0191f829631e9e5af2
Robinrswanson <robinr.swanson@mail.utoronto.ca>
2023-10-09 16:06:43 -0400

    Added many features. Some are uncompleted

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("scrolled");
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const learnMoreButton = document.querySelector(".learn-more-button");
+
+    learnMoreButton.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        // Scroll to the next section with smooth behavior
+        const nextSection = document.querySelector("#next-section");
+        nextSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
