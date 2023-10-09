640ec58cb311b563a9bfe3b00e1defc0bf61c31a
Robinrswanson <robinr.swanson@mail.utoronto.ca>
2023-10-09 13:08:35 -0400

    Some color rework and added rem spacings

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 1) { // Adjust this value as needed
-        navbar.style.backgroundColor = "#333"; // Change to your desired background color
-        navbar.style.transition = "background-color 0.15s ease-in-out"; // Add this line
+    if (window.scrollY > 1) {
+        navbar.classList.add("scrolled");
     } else {
-        navbar.style.backgroundColor = "transparent";
-        navbar.style.transition = "background-color 0.15s ease-in-out"; // Add this line
+        navbar.classList.remove("scrolled");
     }
 });
