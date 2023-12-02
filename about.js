36364e22d8b57e56f04c65974f012956fd235a7e
Robinrswanson <robinr.swanson@mail.utoronto.ca>
2023-12-02 01:19:45 -0500

    Added new exec photos

--- about.js
+++ about.js
@@ -11,3 +11,15 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el2) => observer2.observe(el2));
